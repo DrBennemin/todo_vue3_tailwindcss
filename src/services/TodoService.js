0a6a9a5067fd9f76bd6bc91cb1e3f4464054065e
Benjamin Krzis <benjamin@elisana.digital>
2020-12-10 14:59:16 +0100

    Post todo

--- src/services/TodoService.js
+++ src/services/TodoService.js
@@ -14,6 +14,7 @@ export default {
         return apiClient.get('/todos')
     },
     postTodo(todo) {
+        console.log(todo)
         return apiClient.post('/todos', todo)
     },
 }
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,15 @@ import TodoService from '../services/TodoService'
 
 export default createStore({
     state: {
-        todos: [],
+        todos: [
+            {
+                id: 1,
+                created_by: 'Zaccaria',
+                created_at: '2020-09-03',
+                todo_text: 'ipsum primis in faucibus orci luctus',
+                todo_done: true,
+            },
+        ],
     },
     mutations: {
         ADD_TODO(state, todo) {
